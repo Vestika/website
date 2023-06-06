9b790a14d3cc4d32bcb8cd6c008b8eb7823cc0d8
dansterenson <dansterenson@gmail.com>
2023-06-06 10:13:46 +0300

    improved colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -55,12 +55,6 @@ export const Hero = () => {
             >
               Get Started
             </div>
-            <div
-              className="w-64 sm:w-52 h-12 rounded-xl font-bold text-white border border-solid  flex justify-center items-center cursor-pointer bg-customDarkBg2 hover:bg-customDarkBg3 border-customPrimary transition"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Live demo
-            </div>
           </div>
         </motion.div>
         <motion.div
